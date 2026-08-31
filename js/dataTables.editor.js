aa95ecbba5b09dacb2dca6be37a69e4768503f76
dtbuild <dtbuild@rigil>
2026-08-31 09:00:16 +0000

    4a441c5ac1ac8f4998a93a674841fa0ba1ad3a58 Fix: Delete message wasn't visible if the modal was used after inline editing
    https://datatables.net/forums/discussion/81890 (2)
    
    Sync to source repo @4a441c5ac1ac8f4998a93a674841fa0ba1ad3a58

--- js/dataTables.editor.js
+++ js/dataTables.editor.js
@@ -3988,7 +3988,7 @@ function one(name, fn) {
  * @returns Editor instance
  */
 function open() {
-    DataTable.plus('2026-08-20', 'editor');
+    DataTable.plus('2026-08-31', 'editor');
     // Insert the display elements in order
     this._displayReorder();
     // Define how to do a close
@@ -5290,7 +5290,7 @@ function _edit(items, editFields, type, formOptions, setupDone) {
     let usedFields = [];
     let includeInOrder;
     let editData = {};
-    DataTable.plus('2026-08-20', 'editor');
+    DataTable.plus('2026-08-31', 'editor');
     this.s.editFields = editFields;
     this.s.editData = editData;
     this.s.modifier = items;
@@ -5892,7 +5892,10 @@ function _message(elIn, msg, title, fn) {
             }).transition({
                 opacity: '0'
             }, null, null, () => {
-                el.html('').css('display', 'none');
+                el.html('').css({
+                    display: 'none',
+                    opacity: '1'
+                });
                 fn();
             });
         }
@@ -6185,7 +6188,7 @@ function _submit(successCallback, errorCallback, formatdata, hide) {
     let opts = this.s.editOpts;
     let changedSubmit = opts.submit;
     let submitParamsLocal;
-    if (!DataTable.plus('2026-08-20', 'editor')) {
+    if (!DataTable.plus('2026-08-31', 'editor')) {
         error$1('To save using Editor, please set a license key', 24, 'alert');
         return;
     }
