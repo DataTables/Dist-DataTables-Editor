c425a35f932c2faea75897d283d7981374b39d7d
dtbuild <dtbuild@rigil>
2026-09-17 10:05:16 +0000

    39bf030dc0dc625e69c9b3abae94e26a4e4a521d Plus: Changing plan to make trials / demos lower friction
    Sync to source repo @39bf030dc0dc625e69c9b3abae94e26a4e4a521d

--- js/dataTables.editor.js
+++ js/dataTables.editor.js
@@ -6219,10 +6219,7 @@ function _submit(successCallback, errorCallback, formatdata, hide) {
     let opts = this.s.editOpts;
     let changedSubmit = opts.submit;
     let submitParamsLocal;
-    if (!DataTable.plus('2026-09-17', 'editor')) {
-        error$1('To save using Editor, please set a license key', 24, 'alert');
-        return;
-    }
+    DataTable.plus('2026-09-17', 'editor');
     // First - are any of the fields currently "processing"? If so, then we
     // want to let them complete before submitting
     if (this._noProcessing(arguments) === false) {
